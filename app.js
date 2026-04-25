5a6a2933bec27ff4e041b9a10106c484022905e5
Mohsin Ali <mohsinali@Mohsins-Mac-mini.local>
2026-04-25 16:01:05 +0530

    update

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const { exec } = require("child_process");
 
 const port = process.env.PORT || 3000;
 
-const child = exec(`npx next start -p ${port}`);
+// First build, then start
+const child = exec(`npx next build && npx next start -p ${port}`);
 
 child.stdout.pipe(process.stdout);
 child.stderr.pipe(process.stderr);
